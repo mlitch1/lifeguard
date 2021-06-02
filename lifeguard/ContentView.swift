0dbc46c3a1595e46d4ddfc109d0964a3cf56586f
Michael Litchfield <mike@litch.net>
2021-06-02 14:26:07 -0500

    Initial Commit

---
+++ lifeguard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lifeguard
+//
+//  Created by Michael Litchfield Temp on 6/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lifeguard/lifeguardApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeguardApp.swift
+//  lifeguard
+//
+//  Created by Michael Litchfield Temp on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct lifeguardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
